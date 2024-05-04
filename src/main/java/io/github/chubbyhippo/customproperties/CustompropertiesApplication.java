93e7915a302a5ff94a3ad5ec0c0eabd03a8d4520
ch <mkmsacc@outlook.com>
2024-05-04 16:35:12 +0700

    Initial project setup with Maven, Spring Boot and custom files
    This commit sets up the initial file structure for a Spring Boot project using Maven. It includes the creation of a Maven wrapper for building the project, a Spring Boot application entry point, application properties, and basic project metadata in a pom.xml file. In addition, gitignore rules for the Java ecosystem and various IDEs are defined.

---
+++ src/main/java/io/github/chubbyhippo/customproperties/CustompropertiesApplication.java
@@ -0,0 +1,13 @@
+package io.github.chubbyhippo.customproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustompropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustompropertiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/chubbyhippo/customproperties/CustompropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.chubbyhippo.customproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustompropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
