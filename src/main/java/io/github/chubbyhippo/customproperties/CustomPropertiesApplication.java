604cf2a97d42ec11c58c452f5c200ad54955e027
ch <mkmsacc@outlook.com>
2024-05-04 16:38:36 +0700

    Rename Customproperties classes to use camel case
    The class names CustompropertiesApplication and CustompropertiesApplicationTests have been updated to CustomPropertiesApplication and CustomPropertiesApplicationTests respectively, for better readability and adherence to Java naming conventions.

--- src/main/java/io/github/chubbyhippo/customproperties/CustomPropertiesApplication.java
+++ src/main/java/io/github/chubbyhippo/customproperties/CustomPropertiesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustompropertiesApplication {
+public class CustomPropertiesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustompropertiesApplication.class, args);
+		SpringApplication.run(CustomPropertiesApplication.class, args);
 	}
 
 }
--- src/test/java/io/github/chubbyhippo/customproperties/CustomPropertiesApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/customproperties/CustomPropertiesApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustompropertiesApplicationTests {
+class CustomPropertiesApplicationTests {
 
 	@Test
 	void contextLoads() {
