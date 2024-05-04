0bc02354d48318894057ae410e09e30d79ee26e7
ch <mkmsacc@outlook.com>
2024-05-04 17:05:16 +0700

    Enhance CustomPropertiesApplicationTests with dynamic property change testing
    The previous test case for loading context has been replaced by a new test case. This test validates the functionality of changing property values dynamically at runtime. It utilizes the ConfigurableApplicationContext to manipulate the environment properties for the purpose of the test.

--- src/test/java/io/github/chubbyhippo/customproperties/CustomPropertiesApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/customproperties/CustomPropertiesApplicationTests.java
@@ -1,13 +1,41 @@
 package io.github.chubbyhippo.customproperties;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@TestPropertySource(properties = {"custom.property=default"})
 class CustomPropertiesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ConfigurableApplicationContext context;
 
+    @Test
+    @DisplayName("should change property dynamically")
+    void shouldChangePropertyDynamically() {
+
+        //Get application properties
+        MutablePropertySources propertySources = context.getEnvironment().getPropertySources();
+        assertThat(context.getEnvironment()
+                .getProperty("custom.property")).isEqualTo("default");
+
+        //Change custom.property value
+        Map<String, Object> map = new HashMap<>();
+        map.put("custom.property", "new-value");
+
+        propertySources.addFirst(new MapPropertySource("customMAP", map));
+        assertThat(context.getEnvironment()
+                .getProperty("custom.property")).isEqualTo("new-value");
+    }
 }
